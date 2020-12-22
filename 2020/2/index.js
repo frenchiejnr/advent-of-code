ae0bd481016404b560970baa1bfd573bca5f26a1
frenchiejnr <frenchiejnr@gmail.com>
2020-12-22 16:18:37 +0000

    2020-2 Part 2

--- 2020/2/index.js
+++ 2020/2/index.js
@@ -18,6 +18,20 @@ Each line gives the password policy and then the password. The password policy i
 In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
 
 How many passwords are valid according to their policies?
+
+--- Part Two ---
+While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+How many passwords are valid according to the new interpretation of the policies?
 */
 
 const passwordSpreadsheet = [
@@ -1022,51 +1036,99 @@ const passwordSpreadsheet = [
   "11-15 k: kkkkdkkkkkgkkkkkkrq",
   "9-14 h: hzhhfhhxhhhhhltnh",
 ];
-let validPasswordCount = 0;
 
 let answer = part1Answer();
 console.log(`Part 1 Answer: ${answer}`);
 
-
+answer = part2Answer();
+console.log(`Part 2 Answer: ${answer}`);
 
 function part1Answer() {
-    for (let i = 0; i < passwordSpreadsheet.length; i++) {
-        let split = splitPasswordAndRule(passwordSpreadsheet[i]);
-        let password = split[0];
-        let ruleLetter = split[1];
-        let ruleMin = split[2];
-        let ruleMax = split[3];
-        let validPassword = isValidPassword(password, ruleLetter, ruleMin, ruleMax);
-        if (validPassword === true) {
-            validPasswordCount++;
-        }
+  let validPasswordCount = 0;
+  for (let i = 0; i < passwordSpreadsheet.length; i++) {
+    let split = splitPasswordAndRule(passwordSpreadsheet[i]);
+    let password = split[0];
+    let ruleLetter = split[1];
+    let ruleMin = split[2];
+    let ruleMax = split[3];
+    let validPassword = isValidPasswordPt1(
+      password,
+      ruleLetter,
+      ruleMin,
+      ruleMax
+    );
+    if (validPassword === true) {
+      validPasswordCount++;
     }
-    return validPasswordCount;
+  }
+  return validPasswordCount;
 }
 
-function isValidPassword(password, ruleLetter, ruleMin, ruleMax) {
-    let valid = false;
-    let ruleLetterRegEx = new RegExp(ruleLetter, "g")
-    if (!password.match(ruleLetterRegEx) ) {
-        return valid;
+function part2Answer() {
+  let validPasswordCount = 0;
+  for (let i = 0; i < passwordSpreadsheet.length; i++) {
+    let split = splitPasswordAndRule(passwordSpreadsheet[i]);
+    let password = split[0];
+    let ruleLetter = split[1];
+    let position1 = split[2];
+    let position2 = split[3];
+    let validPassword = isValidPasswordPt2(
+      password,
+      ruleLetter,
+      position1,
+      position2
+    );
+    if (validPassword === true) {
+      validPasswordCount++;
     }
-    
-    let count = password.match(ruleLetterRegEx).length;
-    if (count >= ruleMin && count <= ruleMax) {
-        valid = true;
-        return valid;
-    }
-    else {
-        return valid;
-    }
+  }
+  return validPasswordCount;
 }
+
+
+
+function isValidPasswordPt1(password, ruleLetter, ruleMin, ruleMax) {
+  let valid = false;
+  let ruleLetterRegEx = new RegExp(ruleLetter, "g");
+  if (!password.match(ruleLetterRegEx)) {
+    return valid;
+  }
+
+  let count = password.match(ruleLetterRegEx).length;
+  if (count >= ruleMin && count <= ruleMax) {
+    valid = true;
+    return valid;
+  } else {
+    return valid;
+  }
+}
+
+function isValidPasswordPt2(password, ruleLetter, position1, position2) {
+    let valid = false;
+    if (
+      password[position1 - 1] === ruleLetter &&
+      password[position2 - 1] === ruleLetter
+    ) {
+      valid = false;
+    } else if (
+      password[position1 - 1] !== ruleLetter &&
+      password[position2 - 1] !== ruleLetter
+    ) {
+      valid = false;
+    } else if (password[position1 - 1] === ruleLetter) {
+      valid = true;
+    } else if (password[position2 - 1] === ruleLetter) {
+      valid = true;
+    }
+    return valid;
+  }
 
 function splitPasswordAndRule(password) {
-    let splitPassword = password.split(':');
-    let userPassword = splitPassword[1].trim();
-    let splitRuleAndPassword = splitPassword[0].split(' ');
-    let letter = splitRuleAndPassword[1];
-    let ruleMin = splitRuleAndPassword[0].split('-')[0];
-    let ruleMax = splitRuleAndPassword[0].split('-')[1];
-    return [userPassword, letter, ruleMin, ruleMax];
+  let splitPassword = password.split(":");
+  let userPassword = splitPassword[1].trim();
+  let splitRuleAndPassword = splitPassword[0].split(" ");
+  let letter = splitRuleAndPassword[1];
+  let ruleMin = splitRuleAndPassword[0].split("-")[0];
+  let ruleMax = splitRuleAndPassword[0].split("-")[1];
+  return [userPassword, letter, ruleMin, ruleMax];
 }
