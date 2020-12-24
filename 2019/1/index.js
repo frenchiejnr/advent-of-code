f1f87bcc6132e530ecffaed4823d8f37fc7057f6
frenchiejnr <frenchiejnr@gmail.com>
2020-12-24 17:05:14 +0000

    2019-1 Part 2

--- 2019/1/index.js
+++ 2019/1/index.js
@@ -19,6 +19,19 @@ For a mass of 100756, the fuel required is 33583.
 The Fuel Counter-Upper needs to know the total fuel requirement. To find it, individually calculate the fuel needed for the mass of each module (your puzzle input), then add together all the fuel values.
 
 What is the sum of the fuel requirements for all of the modules on your spacecraft?
+
+--- Part Two ---
+
+During the second Go / No Go poll, the Elf in charge of the Rocket Equation Double-Checker stops the launch sequence. Apparently, you forgot to include additional fuel for the fuel you just added.
+
+Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and subtract 2. However, that fuel also requires fuel, and that fuel requires fuel, and so on. Any mass that would require negative fuel should instead be treated as if it requires zero fuel; the remaining mass, if any, is instead handled by wishing really hard, which has no mass and is outside the scope of this calculation.
+
+So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just calculated as the input mass and repeat the process, continuing until a fuel requirement is zero or negative. For example:
+
+A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0, which would call for a negative fuel), so the total fuel required is still just 2.
+At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel? (Calculate the fuel requirements for each module separately, then add them all up at the end.)
 */
 
 const moduleMasses = [
@@ -124,15 +137,36 @@ const moduleMasses = [
   70673,
 ];
 
-function fuelNeededForModule(module) {
-  let fuelNeeded = Math.floor(module / 3) - 2;
+function fuelNeededForFuel(fuelMass) {
+  let total = fuelNeeded(fuelMass);
+  let totalFuel = total;
+  do {
+    total = fuelNeeded(total);
+    if (total > 0) {
+      totalFuel += total;
+    }
+  } while (Math.floor(total) >= 1);
+  return totalFuel;
+}
+
+function fuelNeeded(mass) {
+  let fuelNeeded = Math.floor(mass / 3) - 2;
   return fuelNeeded;
 }
 function calculateTotalFuel(modules) {
   let totalFuel = 0;
   modules.forEach((module) => {
-    totalFuel += fuelNeededForModule(module);
+    totalFuel += fuelNeeded(module);
+  });
+  return totalFuel;
+}
+
+function calculateTotalFuelWithFuel(modules) {
+  let totalFuel = 0;
+  modules.forEach(module => {
+    totalFuel += fuelNeededForFuel(module);
   });
   return totalFuel;
 }
 console.log(calculateTotalFuel(moduleMasses));
+console.log(calculateTotalFuelWithFuel(moduleMasses));
