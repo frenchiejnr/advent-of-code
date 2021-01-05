bb434d4f422f6acc7cc7a23e5af116d10fa334ec
frenchiejnr <frenchiejnr@gmail.com>
2021-01-05 16:10:59 +0000

    2018-01-2

--- 2018/1/index.test.js
+++ 2018/1/index.test.js
@@ -1,19 +1,36 @@
-const calculateFrequency = require("./index");
+const advent2018_1 = require("./index");
 
 test('all positive', () => {
     let input = [+1, +1, +1]
-    expect(calculateFrequency(input)).toBe(3);
+    expect(advent2018_1.calculateFrequency(input)).toBe(3);
 })
 test('with negative', () => {
     let input = [+1, +1, -2]
-    expect(calculateFrequency(input)).toBe(0);
+    expect(advent2018_1.calculateFrequency(input)).toBe(0);
 })
 test('all negative', () => {
     let input = [-1, -2, -3]
-    expect(calculateFrequency(input)).toBe(-6);
+    expect(advent2018_1.calculateFrequency(input)).toBe(-6);
 })
 test('mixture', () => {
     let input = [+1, -2, +3, +1]
-    expect(calculateFrequency(input)).toBe(3);
+    expect(advent2018_1.calculateFrequency(input)).toBe(3);
+})
+
+test('sameFrequency 1', () => {
+    let input = [+1, -1]
+    expect(advent2018_1.repeatedFrequency(input)).toBe(0);
+})
+test('sameFrequency 2', () => {
+    let input = [+3, +3, +4, -2, -4]
+    expect(advent2018_1.repeatedFrequency(input)).toBe(10);
+})
+test('sameFrequency 3', () => {
+    let input = [-6, +3, +8, +5, -6]
+    expect(advent2018_1.repeatedFrequency(input)).toBe(5);
+})
+test('sameFrequency 4', () => {
+    let input = [+7, +7, -2, -7, -4]
+    expect(advent2018_1.repeatedFrequency(input)).toBe(14);
 })
 
