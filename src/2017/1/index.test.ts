932e37f41dc479ebd02629b77ab5e58ba8a43901
frenchiejnr <frenchiejnr@gmail.com>
2021-01-21 16:11:22 +0000

    2017-01-2

--- src/2017/1/index.test.ts
+++ src/2017/1/index.test.ts
@@ -1,4 +1,4 @@
-import { sumNextDigit } from "./index";
+import { sumNextDigit, sumCircularDigit } from "./index";
 describe('part 1 tests', () => {
     it('should count all matching digits', () => {
     expect(sumNextDigit(1122)).toBe(3);      
@@ -11,5 +11,22 @@ describe('part 1 tests', () => {
     });
     it('should rollover from last digit to first', () => {
         expect(sumNextDigit(91212129)).toBe(9);
+    });
+});
+describe('part 2 tests', () => {
+    it('should', () => {
+    expect(sumCircularDigit(1212)).toBe(6);      
+    });
+    it('should', () => {
+        expect(sumCircularDigit(1221)).toBe(0);
+    });
+    it('should', () => {
+        expect(sumCircularDigit(123425)).toBe(4);
+    });
+    it('should', () => {
+        expect(sumCircularDigit(123123)).toBe(12);
+    });
+    it('should', () => {
+        expect(sumCircularDigit(12131415)).toBe(4);
     });
 });
